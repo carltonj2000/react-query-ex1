e9dd98ec71667fed750dc1f91c98edbb7a0edf5b
Carlton Joseph <carlton.joseph@gmail.com>
2023-04-24 14:59:31 -0700

    commit befor refactor

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,22 @@ function wait(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// /post => ["posts"]
+// /post/1 => ["posts", post.id]
+// /post?authorId=1 => ["posts", {authorId: 1}]
+// /post/2/comments => ["posts", post.id, "comments"]
+
 function App() {
   // console.log(POSTS);
   const queryClient = useQueryClient();
 
   const postsQuery = useQuery({
     queryKey: ["posts"],
-    queryFn: () => wait(1000).then(() => [...POSTS]),
+    queryFn: (obj) =>
+      wait(1000).then(() => {
+        console.log({ obj });
+        return [...POSTS];
+      }),
     // queryFn: () => Promise.reject("Error"),
   });
 
